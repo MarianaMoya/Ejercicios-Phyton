daae9c5df3cdf6b6349df23a0b2392d67650ff06
MarianaMoya <marianamoya6002@gmail.com>
2021-09-24 14:42:32 -0300

    Ejericio: Datos Personales

---
+++ DatosPersonales.py
@@ -0,0 +1,23 @@
+#declaracion de variables
+nombre = ""
+edad = 0
+lug_nac = ""
+
+#ingrese el nombre 
+nombre = input ("Ingrese su nombre: " )
+
+#saludo
+print ("\nBienvenido/a"  ,nombre  ) 
+
+#ingrese lugar de nacimiento
+edad = int (input(" Ingrese su edad :  " ))
+lug_nac = input (" Ingrese su lugar de nacimiento: ")
+
+#muestra los datos ingresados
+print ("\nA continuación confirmaremos los datos")
+print (" Nombre : ", nombre)
+print(" Edad: ",edad)
+print(" Lugar de nacimiento: ",lug_nac)
+
+
+#\n
