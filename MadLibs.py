19100331ec02faf19db8eda7e5fbc15729a5c7d6
Mariana Moya <50000647+MarianaMoya@users.noreply.github.com>
2021-11-16 20:53:57 -0300

    Mad Libs!

---
+++ MadLibs.py
@@ -0,0 +1,7 @@
+color = input ("Ingresa un color: ")
+plural_noun = input ("Ingresa un sustantivo en plural: ")
+celebrity = input ("Ingresa el nombre de una celebridad: ")
+
+print ("Las rosas son " + color)
+print (plural_noun + " son azules")
+print ("Yo amo a " + celebrity )
